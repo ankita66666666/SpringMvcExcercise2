ed3686659db86316c54884fb1d5c4334e55e962d
Ankita <kumariankita.gaurav@cgi.com>
2019-07-31 13:07:21 +0530

    completed Excercise2

---
+++ src/main/java/com/stackroute/controller/AppController.java
@@ -0,0 +1,31 @@
+package com.stackroute.controller;
+
+import com.stackroute.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+//
+//import static org.graalvm.compiler.options.OptionType.User;
+
+@Controller
+
+public class AppController {
+
+
+    @RequestMapping(value = "/")
+    public String greeting(ModelMap map) {
+        return "index";
+    }
+
+    @RequestMapping(value = "/success")
+    public String success(@RequestParam("username") String username, @RequestParam("password") String passsword, ModelMap map) {
+        User user = new User();
+        user.setName(username);
+        map.addAttribute("user", user);
+        return "success";
+    }
+
+}
+
+
